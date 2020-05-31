b51e069d9b69741c687d9563b24c913768f5dbf4
Zach Schiff <zaschiff@gmail.com>
2020-05-31 08:25:02 -0600

    updated notes. In middle of lucky number challenge

---
+++ MyWork/Sec4 - ControlFlowandErrorHandling/luckyNum/main.go
@@ -0,0 +1,8 @@
+/*
+*	Title:				luckyNum/main.go
+*	Course (optional):	GO (Golang): The Complete Bootcamp By Jose Portilla and Inanc Gumus - Udemy Online
+*	Program Author:		Zach Schiff
+*	Date:				2020-05-30
+*	Version:			1.0
+*	Description:		a simple lucky number guessing game. Game is played at the command line.
+*/
